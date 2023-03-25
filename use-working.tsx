39fcf4f87899782da2ce81cf100819b26c212003
Slawa Pidgorny <spidgorny@gmail.com>
2023-03-25 13:02:05 +0100

    Create use-working.tsx

---
+++ use-working.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export function useWorking(defaultValue = false) {
+  const [isWorking, setWorking] = useState(defaultValue);
+
+  const wrapWorking = (code: (e: any) => Promise<any>) => {
+    return async (e: any) => {
+      setWorking(true);
+      const result = await code(e);
+      setWorking(false);
+      return result;
+    };
+  };
+
+  return { isWorking, setWorking, wrapWorking };
+}
