72076c5ae90290badbb2441524d07eb41988a9c3
Slawa Pidgorny <spidgorny@gmail.com>
2023-03-28 15:42:39 +0200

    Create use-update-every-second.tsx

---
+++ use-update-every-second.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+
+export function UpdateEverySecond({ children, refreshInterval = 1000 }) {
+	const [_, setForceRefresh] = useState(0);
+
+	useEffect(() => {
+		const timer = setInterval(() => {
+			setForceRefresh((_) => _ + 1);
+		}, refreshInterval);
+		return () => clearInterval(timer);
+	}, [refreshInterval]);
+
+	return children;
+}
