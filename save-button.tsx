ffacd4e30f3e31c064921b46d5c04e353e613352
Slawa Pidgorny <spidgorny@gmail.com>
2023-04-02 22:03:55 +0200

    Update save-button.tsx

--- save-button.tsx
+++ save-button.tsx
@@ -1,7 +1,14 @@
 import { Button, Spinner } from "react-bootstrap";
 import { HStack } from "./hstack";
+import {ReactNode} from 'react';
 
-export function SaveButton({ children, type, disabled, isWorking, onClick, className }) {
+export function SaveButton({ children, type, disabled, isWorking, onClick, className }:{
+ children: ReactNode; 
+ type?: "button"|"submit"|"reset"; 
+ disabled?: boolean; 
+ isWorking?: boolean; 
+ onClick?: () => void; className?: string;
+}) {
 	return (
 		<Button type={type} disabled={disabled} onClick={onClick} className={className}>
 			<HStack>
