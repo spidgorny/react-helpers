0581f2d4e6a48483d62ab05ed407120c9299bd35
Slawa Pidgorny <spidgorny@gmail.com>
2023-04-03 23:32:32 +0200

    Update console-log.ts

--- lib/console-log.ts
+++ lib/console-log.ts
@@ -1,17 +1,20 @@
-export function processStdoutWrite(...vars) {
+export function processStdoutWrite(...vars: any[]) {
 	if (typeof process.stdout.write === 'function') {
+		// @ts-ignore
 		process.stdout.write(...vars)
 	}
 }
 
-export function processStdoutClearLine(...vars) {
+export function processStdoutClearLine(...vars: any[]) {
 	if (typeof process.stdout.clearLine === 'function') {
+		// @ts-ignore
 		process.stdout.clearLine(...vars)
 	}
 }
 
-export function processStdoutCursorTo(...vars) {
+export function processStdoutCursorTo(...vars: any[]) {
 	if (typeof process.stdout.cursorTo === 'function') {
+		// @ts-ignore
 		process.stdout.cursorTo(...vars)
 	}
 }
