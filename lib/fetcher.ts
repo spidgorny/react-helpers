13944119585f2735666396ffea2461b2e28e1407
Slawa Pidgorny <spidgorny@gmail.com>
2023-04-03 23:44:30 +0200

    Update fetcher.ts

--- lib/fetcher.ts
+++ lib/fetcher.ts
@@ -1,10 +1,10 @@
+import {ErrorWithDetails} from './error';
+
 export const fetcher = async (url) => {
 	const res = await fetch(url);
 	if (!res.ok) {
 		let jsonResponse = await res.json();
-		const error = new Error(jsonResponse?.message ?? "An error occurred while fetching the data.");
-		error.info = jsonResponse;
-		error.status = res.status;
+		let error = new ErrorWithDetails(jsonResponse?.message ?? "An error occurred while fetching the data.", jsonResponse);
 		throw error;
 	}
 
