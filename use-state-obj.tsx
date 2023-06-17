179ece60378a91a174fe20e7df2f3d10aa11fde0
Slawa <spidgorny@gmail.com>
2023-06-17 23:48:29 +0200

    improvements

--- use-state-obj.tsx
+++ use-state-obj.tsx
@@ -1,8 +1,8 @@
+"use client";
+
 import { ChangeEvent, ChangeEventHandler, useState } from "react";
 
-export function useStateObj<T>(
-	defaultValue: T | undefined = undefined
-): StateObj<T> {
+export function useStateObj<T>(defaultValue: T | undefined = undefined): StateObj<T> {
 	const [value, set] = useState<T | undefined>(defaultValue);
 	const increment = () => {
 		set((v) => {
--- use-working.tsx
+++ use-working.tsx
@@ -1,16 +1,17 @@
+"use-client";
 import { useState } from "react";
 
 export function useWorking(defaultValue = false) {
-  const [isWorking, setWorking] = useState(defaultValue);
+	const [isWorking, setWorking] = useState(defaultValue);
 
-  const wrapWorking = (code: (e: any) => Promise<any>) => {
-    return async (e: any) => {
-      setWorking(true);
-      const result = await code(e);
-      setWorking(false);
-      return result;
-    };
-  };
+	const wrapWorking = (code: (e: any) => Promise<any>) => {
+		return async (e: any) => {
+			setWorking(true);
+			const result = await code(e);
+			setWorking(false);
+			return result;
+		};
+	};
 
-  return { isWorking, setWorking, wrapWorking };
+	return { isWorking, setWorking, wrapWorking };
 }
