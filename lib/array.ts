229f91c8a9d5296f9e62733de2c2930c5eca547e
Slawa Pidgorny <spidgorny@gmail.com>
2024-03-14 09:46:11 +0100

    add types to array.ts

--- lib/array.ts
+++ lib/array.ts
@@ -1,5 +1,5 @@
 export function uniqueStrings(strList: string[]) {
-	return strList.reduce((a, x) => (a.includes(x) ? a : [...a, x]), []);
+	return strList.reduce((a, x) => (a.includes(x) ? a : [...a, x]), [] as string[]);
 }
 
 export function uniqueObjectByKey(objList: any[], key = "id") {
@@ -9,7 +9,7 @@ export function uniqueObjectByKey(objList: any[], key = "id") {
 	);
 }
 
-export const sortParts = (a, b) => {
+export const sortParts = (a: any, b: any) => {
 	for (let i in a) {
 		let aPart = a[i];
 		let bPart = b[i];
@@ -22,7 +22,7 @@ export const sortParts = (a, b) => {
 	return 0;
 };
 
-export function array_same(a1, a2) {
+export function array_same(a1: any[], a2: any[]) {
 	return a1.every((x) => a2.includes(x)) && a2.every((x) => a1.includes(x));
 }
 
@@ -30,27 +30,27 @@ export const range = function* (from = 0, total = 0, step = 1) {
 	for (let i = 0; i < total; yield from + i++ * step) {}
 };
 
-export function rangeBetween(from, till, step = 1) {
+export function rangeBetween(from: number, till: number, step = 1) {
 	return range(from, till - from + 1, step);
 }
 
-export function arraySum(list = [], selector) {
+export function arraySum(list = [], selector: (x: any) => number) {
 	return list.reduce((a, x) => a + selector(x), 0);
 }
 
-export function arraySort(items, selector) {
+export function arraySort(items: any[], selector: (x: any) => number) {
 	items = items?.sort(sortBy(selector));
 	return items;
 }
 
-export function arraySorBToA(items, selector) {
+export function arraySorBToA(items: any[], selector: (x: any) => number) {
 	items = items.sort(sortBy(selector));
 	items = items.reverse();
 	return items;
 }
 
 export function sortBy(selector: (row: any) => number) {
-	return (a, b) => {
+	return (a: any, b: any) => {
 		a = selector(a);
 		b = selector(b);
 		if (typeof a === "number" && typeof b === "number") {
@@ -65,18 +65,18 @@ export function sortBy(selector: (row: any) => number) {
 	};
 }
 
-export function arrayWithout(array, selector) {
+export function arrayWithout(array: any[], selector: (x: any) => boolean) {
 	return array.filter((x) => !selector(x));
 }
 
-export function arrayUnique(array, selector) {
+export function arrayUnique(array: any[], selector: (x: any) => any) {
 	const uniqSet = [...new Set(array.map(selector))];
 	return uniqSet.map((uniqItem) => {
 		return array.find((x) => selector(x) === uniqItem);
 	});
 }
 
-export function groupBy(array = [], selector) {
+export function groupBy(array = [], selector: (x: any) => any) {
 	const uniqSet = [...new Set(array.map(selector))];
 	const pairs = uniqSet.map((uniqItem) => {
 		let items = array.filter((x) => selector(x) === uniqItem);
